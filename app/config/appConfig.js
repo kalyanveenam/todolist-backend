ad1ef50277decc56812c695e1a975016e21adf9b
kalyanveenam <55013598+kalyanveenam@users.noreply.github.com>
2020-09-05 00:56:10 -0700

    added config

---
+++ app/config/appConfig.js
@@ -0,0 +1,12 @@
+let config = {
+  apiVersion: "/api/v1",
+  PORT: process.env.PORT || 3001,
+
+  mongodb: {
+    produrl: "mongodb+srv://admin:admin123@todolist.luwaj.mongodb.net/todolist?retryWrites=true&w=majority",
+  localurl:"mongodb://localhost:27017/todolist"
+  },
+  env: "prod",
+};
+
+module.exports = { config: config };
