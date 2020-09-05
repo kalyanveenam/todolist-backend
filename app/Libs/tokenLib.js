453d88ef9ed1986b408350a342603689323108bc
kalyanveenam <55013598+kalyanveenam@users.noreply.github.com>
2020-09-04 23:44:14 -0700

    signin routes

---
+++ app/Libs/tokenLib.js
@@ -0,0 +1,17 @@
+let jwt = require("jsonwebtoken");
+let secret = process.env.JWT_SECRET;
+let generateToken = (data, callback) => {
+  try {
+    
+    var tokenDetails = {
+      token: jwt.sign(data.toJSON(), secret),
+      secret: secret,
+    };
+    callback(null, tokenDetails);
+  } catch (err) {
+    callback(err, null);
+  }
+};
+module.exports = {
+  generateToken: generateToken,
+};
